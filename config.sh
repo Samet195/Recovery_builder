b0475ce2d63301ea2b36da272c6231f184a0d2f0
Efe Timur Turan <tasalxntn@gmail.com>
2023-11-08 18:43:28 +0300

    Update config.sh

--- config.sh
+++ config.sh
@@ -2,16 +2,17 @@
 
 # about the custom recovery
 export NAME="twrp" # supported: pbrp, shrp, twrp
-export MANIFEST="https://github.com/minimal-manifest-twrp/platform_manifest_twrp_omni.git" # the link of manifest
+export MANIFEST="https://github.com/minimal-manifest-twrp/platform_manifest_twrp_aosp.git" # the link of manifest
 export BRANCH="twrp-12.1" # the branch of manifest
 
 # about your device
 export DEVICE="G512" # codename used in device tree
-export DT_LINK="https://github.com/ruigegoat/twrp_device_general_G512" # device tree link
+export DT_LINK="https://github.com/ruigegoat/twrp_device_general_G512.git" # device tree link
 export DT_BRANCH="main" # device tree branch
 export VENDOR="general" # device manufacturer or vendor
 export TARGET="bootimage" # build method: recoveryimage, bootimage, vendorboot
 export TYPE="omni" # recovery type: omni, twrp, something else
+export ALLOW_MISSING_DEPENDENCIES=true
 
 # you dont need to edit these
 export SYM="_"
